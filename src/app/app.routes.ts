28f93f2bc5ecf1a343a86f0e54a9876efd3fbee5
Emanuel Urrutia Picarte <eurrutiapc@gmail.com>
2025-04-28 21:12:16 -0400

    02 - update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { Component } from '@angular/core';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+];
